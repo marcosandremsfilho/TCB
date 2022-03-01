4689047a1ec26071d5fcc40cd0e7747c6e05028b
marcosandremsfilho <marcos32andre@gmail.com>
2022-03-01 10:10:31 -0300

    Teste

--- IRLibrary.cpp
+++ IRLibrary.cpp
@@ -3,11 +3,25 @@
 #define pin 2
 
 unsigned long time;
+unsigned long decode;
+unsigned long highSignal;
+unsigned long lowSignal;
+unsigned long signal;
+
+int maquina = 0;
 
 void setup() {
-    pinMode(pin, OUTPUT);
+    pinMode(pin, INPUT);
 }
+
 void loop(){
+    switch(maquina){
+
+    }
+   
 
 }
 
+void inicial() {
+    signal = digitalRead(pin);
+}
